35924981357dfc35868c2905426436bf8262a4dd
Jaanvi Prabhakar <jaanviprabhakar5@gmail.com>
2025-06-23 00:38:34 -0400

    Initial Project Setup: Spring Webflux + React

---
+++ backend/lockedin/src/main/java/com/jaanvi_prabhakar/lockedin/LockedInApplication.java
@@ -0,0 +1,13 @@
+package com.jaanvi_prabhakar.lockedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockedInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockedInApplication.class, args);
+	}
+
+}
---
+++ backend/lockedin/src/test/java/com/jaanvi_prabhakar/lockedin/LockedInApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaanvi_prabhakar.lockedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockedInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
